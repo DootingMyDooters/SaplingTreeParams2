1b04793a5819f0bcfbedb57df46fd45b4c66513d
DootingMyDooters <viki9400@gmail.com>
2024-02-11 21:49:12 +0200

    created separate class for parameters

---
+++ SaplingTreeParams2/SaplingParameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaplingTreeParams2
+{
+    public class SaplingParameters
+    {
+        public string treeType;
+        public bool skipForestFloor;
+        public float size;
+        public float otherBlockChance;
+        public float vinesGrowthChance;
+        public float mossGrowthChance;
+        public bool ignoreColdTemp;
+        public SaplingParameters() {
+            treeType = "pine";
+            skipForestFloor = true;
+            size = 1;
+            otherBlockChance = 1;
+            vinesGrowthChance = 0.01f;
+            mossGrowthChance = 0.02f;
+            ignoreColdTemp = true;
+        }
+        
+    }
+}
