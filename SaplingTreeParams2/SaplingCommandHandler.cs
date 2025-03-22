274da8a3f81dc4b2483206cbe1790c2c4ee4af97
DootingMyDooters <viki9400@gmail.com>
2025-03-22 13:40:24 +0200

    added new listtreetypes sub command to sapconfig command

--- SaplingTreeParams2/SaplingCommandHandler.cs
+++ SaplingTreeParams2/SaplingCommandHandler.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Util;
 
 namespace SaplingTreeParams2
@@ -134,6 +135,15 @@ namespace SaplingTreeParams2
         public TextCommandResult printConfig()
         {
             return TextCommandResult.Success("full config:\n" + String.Join(",\n", config.saplingParameters.Select(sap => sap.prettyString())));
+        }
+
+        public TextCommandResult printTreeTypeList()
+        {
+            List<string> treeTypeList = new List<string>();
+            Lang.GetAllEntries().Keys.Foreach((string key) => {
+                if (key.Contains("treeseed-planted-")) treeTypeList.Add(key.Split("-")[2]);
+            } );
+            return TextCommandResult.Success(String.Join(",", treeTypeList));
         }
     }
 }
